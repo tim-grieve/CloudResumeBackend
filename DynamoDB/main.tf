f9214238bc00381fa8b5260a89dd41460dc53558
Tim <tim.grieve@shellflow.com>
2023-04-04 11:22:23 -0400

    First attempt at creating a DynamoDB table

--- DynamoDB/main.tf
+++ DynamoDB/main.tf
@@ -10,3 +10,23 @@ terraform {
 provider "aws" {
   region  = var.provider_region_id
 }
+
+resource "aws_dynamodb_table" "visitor-count" {
+    name = "visitor_counter"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "count"
+
+    attribute {
+      name = "count"
+      type = "N"
+    }
+
+    ttl {
+        attribute_name = "TimeToExist"
+        enabled = false
+    }
+
+    tags = {
+        Name = "Resume Visitor Counter"
+    }
+}
