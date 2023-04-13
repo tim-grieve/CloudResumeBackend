e68bf0a2ad38c2d071efb9f085817b13a709abf9
Tim <tim.grieve@shellflow.com>
2023-04-13 07:33:34 -0400

    playing with cors settings

--- APIGateway/main.tf
+++ APIGateway/main.tf
@@ -2,15 +2,7 @@ resource "aws_apigatewayv2_api" "counter_api" {
   name = "resume-counter-api"
   protocol_type = "HTTP"
   target = var.lambda_arn
-  route_key = "ANY /${var.lambda_function_name}"
-  
-  cors_configuration {
-    allow_origins = ["*"]
-    allow_methods = ["POST","GET","OPTIONS"]
-    allow_headers = ["Content-Type"]
-    
-    max_age = 300
-  }
+  route_key = "ANY /${var.lambda_function_name}/{proxy+}"
 }
 
 resource "aws_lambda_permission" "apigateway_permissions" {
