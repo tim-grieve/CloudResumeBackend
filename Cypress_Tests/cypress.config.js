b8b0190c0413f927a38fc46aa2791c3852a2b99c
Tim <tim.grieve@shellflow.com>
2023-04-18 11:35:55 -0400

    update config.js

--- Cypress_Tests/cypress.config.js
+++ Cypress_Tests/cypress.config.js
@@ -1,4 +1,8 @@
-module.exports = {
-    projectId: "kjixxa"
+const { defineConfig } = require('cypress')
+module.exports = defineConfig({
+    projectId: "kjixxa",
+    e2e: {
+        baseUrl: 'https://resume.shellflow.com'
+    },
     // The rest of the Cypress config options go here...
-}
+})
