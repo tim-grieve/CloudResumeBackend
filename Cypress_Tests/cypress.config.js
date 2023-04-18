103b3bb23f5d13d7f0af48a8da10a6ace376def4
Tim <tim.grieve@shellflow.com>
2023-04-18 11:44:20 -0400

    Turned off cypress e2e support files

--- Cypress_Tests/cypress.config.js
+++ Cypress_Tests/cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     projectId: "kjixxa",
     e2e: {
-        baseUrl: 'https://resume.shellflow.com'
+        baseUrl: 'https://resume.shellflow.com',
+        supportFile: false
     },
     // The rest of the Cypress config options go here...
 })
