52873052dd60b215909482bddcb961f2c510240b
Tim <tim.grieve@shellflow.com>
2023-04-19 05:36:09 -0400

    Testing assertion errors

--- Lambda/tests.py
+++ Lambda/tests.py
@@ -7,14 +7,15 @@ class aws_connectivity(unittest.TestCase):
     def test_connection(self):
         # Create a DynamoDBResource
         connection = Dynamodb_Resource(aws_region_name,dynamodb_table_name,dynamodb_table_key,dynamodb_table_value)
+        connection.aws_resource = None
         # Confirm the resource can successfully connect to AWS
-        self.assertIsNotNone(connection.aws_resource)
+        self.assertIsNotNone(connection.aws_resource, 'Connection to dynamodb failed')
 
     def test_count_value(self):
         connection = Dynamodb_Resource(aws_region_name, dynamodb_table_name, dynamodb_table_key, dynamodb_table_value)
         count = connection.get_count()['Item']['Count']
 
-        self.assertIsInstance(count, decimal.Decimal)
+        self.assertIsInstance(count, decimal.Decimal, 'Count is not a Decimal')
 
     def test_increment_count(self):
         connection = Dynamodb_Resource(aws_region_name, dynamodb_table_name, dynamodb_table_key, dynamodb_table_value)
@@ -26,6 +27,6 @@ class aws_connectivity(unittest.TestCase):
 
 tests = aws_connectivity()
 
-tests.test_connection
-tests.test_count_value
-tests.test_increment_count
+print(tests.test_connection())
+tests.test_count_value()
+tests.test_increment_count()
