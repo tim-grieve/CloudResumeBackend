48e51846c90558a6cbb1e1a9fca41b86693e1e07
Tim <tim.grieve@shellflow.com>
2023-04-19 05:10:29 -0400

    Test python 3

--- Lambda/tests.py
+++ Lambda/tests.py
@@ -7,7 +7,6 @@ class aws_connectivity(unittest.TestCase):
     def test_connection(self):
         # Create a DynamoDBResource
         connection = Dynamodb_Resource(aws_region_name,dynamodb_table_name,dynamodb_table_key,dynamodb_table_value)
-        
         # Confirm the resource can successfully connect to AWS
         self.assertIsNotNone(connection.aws_resource)
 
