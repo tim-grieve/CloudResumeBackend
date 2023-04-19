c4b553def160394ee2c283447bf6a07a51e40ecd
Tim <tim.grieve@shellflow.com>
2023-04-19 05:42:38 -0400

    Removed failed test values

--- Lambda/tests.py
+++ Lambda/tests.py
@@ -3,11 +3,10 @@ import decimal
 from lambdafunction import Dynamodb_Resource, aws_region_name, dynamodb_table_name, dynamodb_table_key, dynamodb_table_value
 
 
-class aws_connectivity(unittest.TestCase):
+class AwsConnectivity(unittest.TestCase):
     def test_connection(self):
         # Create a DynamoDBResource
         connection = Dynamodb_Resource(aws_region_name,dynamodb_table_name,dynamodb_table_key,dynamodb_table_value)
-        connection.aws_resource = None
         # Confirm the resource can successfully connect to AWS
         self.assertIsNotNone(connection.aws_resource, 'Connection to dynamodb failed')
 
@@ -21,12 +20,11 @@ class aws_connectivity(unittest.TestCase):
         connection = Dynamodb_Resource(aws_region_name, dynamodb_table_name, dynamodb_table_key, dynamodb_table_value)
         count = connection.get_count()['Item']['Count']
         new_count = connection.increment_count()
+        self.assertTrue(new_count - count == 1, "Count not incrementing by 1")
 
-        self.assertTrue(new_count - count == 1, "Count incremented by 1")
+# Run tests
+tests = AwsConnectivity()
 
-
-tests = aws_connectivity()
-
-print(tests.test_connection())
+tests.test_connection()
 tests.test_count_value()
 tests.test_increment_count()
